9739f56b590adc0ba2fa9c287631ac2e6fe81dfa
Ayman El Didi <ayman@eldidi.org>
2023-10-23 20:41:22 -0600

    ...

--- c.mod
+++ c.mod
@@ -1,2 +1,3 @@
 module c.eldidi.org/x
 version c11
+os linux -D_POSIX_C_SOURCE=200809L
--- fs/fs.c
+++ fs/fs.c
@@ -1,6 +1,5 @@
 
 #if defined(__linux__) || defined(__APPLE__)
-#define _POSIX_C_SOURCE 200809L
 #include "./fs_posix.c"
 #elif defined(_WIN32)
 #error "TODO"
