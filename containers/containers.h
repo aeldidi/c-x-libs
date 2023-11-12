9a637f9cf1f21f58182c826a0744a132b23166e1
Ayman El Didi <ayman@eldidi.org>
2023-11-12 13:46:01 -0700

    changed hashmap API

--- containers/containers.h
+++ containers/containers.h
@@ -30,99 +30,49 @@ void slice_grow(Arena* mem, void* slice, const size_t size);
 
 // In your structure, these can be any type so long as the types are laid out
 // as in the Map structure.
-#define map_keytype int
+#define map_keytype char*
 #define map_valtype int
 
-// A Map is any struct which has the following members laid out like so:
-// If your keytype is char*, you can omit the hash and equals function, and use
-// map_get instead of hashmap_get.
+// A Map is any struct which has the following members.
 typedef struct Map Map;
 struct Map {
 	Map*        child[4];
 	map_keytype key;
 	map_valtype value;
-	uint64_t (*hash)(map_keytype);
-	bool (*equals)(map_keytype, map_keytype);
 };
 
-#define make_hashmap_get_func(name, maptype, keytype, valtype)                \
-	valtype* name(Arena* arena, maptype** map, keytype k)                 \
-	{                                                                     \
-                                                                              \
-		assert(map != NULL);                                          \
-		for (uint64_t h = map->hash(k); *map; h <<= 2) {              \
-			if (map->equals(k, (*map)->key)) {                    \
-				return &(*map)->value;                        \
-			}                                                     \
-			map = &(*map)->child[h >> 62];                        \
-		}                                                             \
-                                                                              \
-		if (!arena) {                                                 \
-			return NULL;                                          \
-		}                                                             \
-                                                                              \
-		*map        = arena_make(arena, maptype);                     \
-		(*map)->key = k;                                              \
-		return &(*map)->value;                                        \
+// The hash function and equals functions should look something like this.
+typedef uint64_t map_hash_function(map_keytype);
+typedef bool     map_equals_function(map_keytype, map_keytype);
+
+// Here is an example of a map_get_function. Here we use fnv_1a_str as the hash
+// function, since we use char* as the key type and strcmp as the equals
+// function. Feel free to copy and paste this, and modify it for your own
+// types as needed.
+map_valtype*
+map_get_function(Arena* arena, Map** map, map_keytype k)
+{
+	assert(map != NULL);
+	for (uint64_t h = fnv_1a_str(k); *map; h <<= 2) {
+		if ((*map)->key != NULL && strcmp(k, (*map)->key) == 0) {
+			return &(*map)->value;
+		}
+		map = &(*map)->child[h >> 62];
 	}
 
-#define make_map_get_func(name, maptype, keytype, valtype)                    \
-	valtype* name(Arena* arena, maptype** map, keytype k)                 \
-	{                                                                     \
-		assert(map != NULL);                                          \
-		for (uint64_t h = fnv_1a_str(k); *map; h <<= 2) {             \
-			printf("map = %p\n", map);                            \
-			if ((*map)->key != NULL &&                            \
-					strcmp(k, (*map)->key) == 0) {        \
-				printf("found\n");                            \
-				return &(*map)->value;                        \
-			}                                                     \
-			printf("nuh uh\n");                                   \
-			map = &(*map)->child[h >> 62];                        \
-		}                                                             \
-                                                                              \
-		if (!arena) {                                                 \
-			return NULL;                                          \
-		}                                                             \
-                                                                              \
-		printf("adding '%s' at %p", k, map);                          \
-		*map        = arena_make(arena, maptype);                     \
-		(*map)->key = k;                                              \
-		return &(*map)->value;                                        \
+	if (!arena) {
+		return NULL;
 	}
 
-#define hashmap_get(arena, map, k)                                            \
-	({                                                                    \
-		for (uint64_t h = map->hash(k); *map; h <<= 2) {              \
-			if (map->equals(k, (*map)->key)) {                    \
-				return &(*map)->value;                        \
-			}                                                     \
-			map = &(*map)->child[h >> 62];                        \
-		}                                                             \
-		if (!arena) {                                                 \
-			NULL;                                                 \
-		} else {                                                      \
-			*map        = arena_make(arena, map);                 \
-			(*map)->key = key;                                    \
-			&(*map)->value;                                       \
-		}                                                             \
-	})
+	*map        = arena_make(arena, Map);
+	(*map)->key = k;
+	return &(*map)->value;
+}
 
-#define map_get(arena, map, k)                                                \
-	({                                                                    \
-		for (uint64_t h = fnv_1a_str(k); *map; h <<= 2) {             \
-			if (strcmp(k, (*map)->key)) {                         \
-				return &(*map)->value;                        \
-			}                                                     \
-			map = &(*map)->child[h >> 62];                        \
-		}                                                             \
-		if (!arena) {                                                 \
-			NULL;                                                 \
-		} else {                                                      \
-			*map        = arena_make(arena, map);                 \
-			(*map)->key = k;                                      \
-			&(*map)->value;                                       \
-		}                                                             \
-	})
+// Assuming your function has a signature similar to map_get_function, you can
+// use this as a convenience macro similarly to slice_push.
+//
+// This macro assumes the function to get from type Map is Map_get.
+#define map_get(arena, map_type, map, key) map_type##_get(arena, map, key)
 
 #endif // MAP_H
